8d0565218effa6e01b125c976821972653d8d038
Alberto Cabrera <betolcq@gmail.com>
2026-02-13 12:20:10 -0400

    se realizo la conexion a la base de datos, organizo la estrcutura, se añadio una nueva libreria y se la añadio al requirements.txt

---
+++ 01_proyEva/backend/db/connection.py
@@ -0,0 +1,14 @@
+import mysql.connector
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+def get_connection():
+    conn = mysql.connector.connect(
+        host = os.getenv("DB_HOST"),
+        port = os.getenv("DB_PORT"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASS"),
+        database = os.getenv("DB_NAME")
+    )
+    return conn
